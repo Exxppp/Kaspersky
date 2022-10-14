9ec85db6b7da77dc1b6cd58bceb71ba21ced7967
Rodion <Rodion@example.com>
2022-10-14 15:13:15 +0300

    add webdriver-manager

--- conftest.py
+++ conftest.py
@@ -1,22 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def browser_setup():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager(path=r".\\Drivers").install()), options=options)
+    browser.maximize_window()
+    return browser
 
 
 @pytest.fixture(scope="function")
 def browser():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
-    browser.maximize_window()
+    browser = browser_setup()
     yield browser
     browser.quit()
 
 
 @pytest.fixture(scope="class")
 def browser_class():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(options=options)
-    browser.maximize_window()
+    browser = browser_setup()
     yield browser
     browser.quit()
